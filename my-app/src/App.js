1014621cdc40887278a13590207a9423d3456b76
townsend75 Townsend <dtownsend@gmx.net>
2023-11-06 18:52:33 +0000

    add navbarform

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import css from './App.module.css';
-import Sidebar from "./components/Sidebar";
+import NavBarForm from './components/NavBarForm';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <NavBarForm />
     </div>
   );
 }
